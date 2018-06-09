3a8942c23e6fda2530938fc07b277a127008fbc1
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2018-06-09 16:07:23 +0900

    finished chapter 2

---
+++ src/main/java/money/Dollar.java
@@ -0,0 +1,15 @@
+package money;
+
+public class Dollar {
+
+    int amount;
+
+	public Dollar(int amount){
+        this.amount = amount;
+    }
+
+	public Dollar times(int multiplier) {
+        return new Dollar(this.amount * multiplier);
+	}
+
+}
