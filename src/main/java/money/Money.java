2705c6507e733b3efa1a09cd9bc8afbf464ab978
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2018-06-23 22:02:50 +0900

    Finished chapter 8

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,12 +1,22 @@
 package money;
 
-public class Money {
+public abstract class Money {
 
     protected int amount;
 
     public boolean equals(Object o) {
         Money money = (Money) o;
-        return this.amount == money.amount;
+        return this.amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    abstract Money times(int multiplier);
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 
 }
